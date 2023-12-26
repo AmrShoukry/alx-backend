f911fa9a4e521bd97a3ca8a1b29dec4fb86937d6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-26 14:27:34 +0200

    task0 fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,11 +3,12 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ return start and end index """
     start_index = 0
     end_index = page_size
-    
+
     for i in range(1, page):
         start_index += page_size
         end_index += page_size
