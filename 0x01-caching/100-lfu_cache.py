0c799aec04319f81a94bf8eed9deb7dc6c07e034
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 17:00:19 +0200

    task100 fix2

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -18,19 +18,20 @@ class LFUCache(BasicCache):
         cache = self.cache_data
         if (key in cache.keys()):
             cache[key] = item
-            self.priorities[key] = [self.priorities[key][0] + 1, self.priorities[key][1]]
+            self.priorities[key] = [self.priorities[key][0] + 1,
+                                    self.priorities[key][1]]
             self.counter += 1
         elif not (key is None or item is None):
             if len(self.cache_data) >= BasicCache.MAX_ITEMS:
                 first_key = sorted(self.priorities.items(),
-                                   key=lambda item: (item[1][0], item[1][1]))[0][0]
+                                   key=lambda item: (item[1][0],
+                                                     item[1][1]))[0][0]
                 cache.pop(first_key)
                 self.priorities.pop(first_key)
                 print(f"DISCARD: {first_key}")
             self.priorities[key] = [1, self.counter]
             self.counter += 1
             cache[key] = item
-
 
     def get(self, key):
         """ GET an item """
