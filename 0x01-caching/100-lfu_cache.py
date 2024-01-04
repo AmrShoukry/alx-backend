3bc0a010c070331ca9d2f40de8895698b1a30933
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 16:59:32 +0200

    task100

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" MRU Caching """
+""" LFU Caching """
 
 BasicCache = __import__('0-basic_cache').BasicCache
 
 
-class MRUCache(BasicCache):
-    """ MRU Cache class """
+class LFUCache(BasicCache):
+    """ LFU Cache class """
 
     def __init__(self):
         """ Init method """
@@ -18,19 +18,19 @@ class MRUCache(BasicCache):
         cache = self.cache_data
         if (key in cache.keys()):
             cache[key] = item
-            self.priorities[key] = self.counter
+            self.priorities[key] = [self.priorities[key][0] + 1, self.priorities[key][1]]
             self.counter += 1
         elif not (key is None or item is None):
             if len(self.cache_data) >= BasicCache.MAX_ITEMS:
                 first_key = sorted(self.priorities.items(),
-                                   key=lambda item: item[1],
-                                   reverse=True)[0][0]
+                                   key=lambda item: (item[1][0], item[1][1]))[0][0]
                 cache.pop(first_key)
                 self.priorities.pop(first_key)
                 print(f"DISCARD: {first_key}")
-            cache[key] = item
-            self.priorities[key] = self.counter
+            self.priorities[key] = [1, self.counter]
             self.counter += 1
+            cache[key] = item
+
 
     def get(self, key):
         """ GET an item """
