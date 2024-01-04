0d2702f08c7b45e63c47d245ffe32e52f981d9b5
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 16:11:52 +0200

    task1 fix3

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" FIFO Caching """
+""" LIFO Caching """
 
 BasicCache = __import__('0-basic_cache').BasicCache
 
 
-class FIFOCache(BasicCache):
-    """ FIFO Cache class """
+class LIFOCache(BasicCache):
+    """ LIFO Cache class """
 
     def __init__(self):
         """ Init method """
@@ -16,16 +16,20 @@ class FIFOCache(BasicCache):
     def put(self, key, item):
         """ Put an item """
         cache = self.cache_data
-        if not (key is None or item is None or key in cache.keys()):
+        if (key in cache.keys()):
+            self.priorities[key] = self.counter
+            self.counter += 1
+            print(self.priorities)
+        elif not (key is None or item is None):
             if len(self.cache_data) >= BasicCache.MAX_ITEMS:
-                first_key = sorted(self.priorities.items(),
-                                   key=lambda item: item[1])[0][0]
+                first_key = sorted(self.priorities.items(), key=lambda item: item[1], reverse=True)[0][0]
                 cache.pop(first_key)
                 self.priorities.pop(first_key)
                 print(f"DISCARD: {first_key}")
             cache[key] = item
             self.priorities[key] = self.counter
             self.counter += 1
+            print(self.priorities)
 
     def get(self, key):
         """ GET an item """
