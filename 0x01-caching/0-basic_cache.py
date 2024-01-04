fb47158659346447601b1187f48285b7fc2f6460
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 15:25:51 +0200

    task0 fix2

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,12 +8,12 @@ class BasicCache(BaseCaching):
     """ Basic caching class """
 
     def put(self, key, item):
-        """ PUT """
+        """ PUT an item """
         if not (key is None or item is None):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ GET """
+        """ GET an item """
         if (key is None):
             return None
         return self.cache_data.get(key, None)
